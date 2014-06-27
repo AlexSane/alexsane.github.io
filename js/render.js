12de662b6a2efe22a2accee4c102a0a25e69ec42
Alex Fomin <ShurickFomin@gmail.com>
2014-06-27 17:52:11 +0300

    simplify rendering

--- js/render.js
+++ js/render.js
@@ -3,20 +3,12 @@ define(['Underscore', 'Snap', 'constants', 'functions'], function(_, Snap, c, f)
         renderBob: function(snap, start, finish, color){
             var sc = c.points[start];
             var fc = c.points[finish];
-            var format = 'M-{s4},0 A{s4},{s4} 0 0,1 {s4},0 v{s} A{s4},{s4} 0 0,1 -{s4},{s} z';
 
-            return snap.path(Snap.format(format, {
-                s4: c.size / 4,
-                s: f.distance(sc,fc)
-            }))
-                .attr({
-                    fill: color
-                })
-                .transform(Snap.format('translate({x},{y}) rotate({a})', {
-                    x: sc.x,
-                    y: sc.y,
-                    a: Snap.deg(-f.angle(fc, sc))
-                }));
+            return snap.line(sc.x,sc.y, fc.x, fc.y).attr({
+                stroke:color,
+                strokeWidth: c.size/2,
+                strokeLinecap:"round"
+            });
         },
 
 
